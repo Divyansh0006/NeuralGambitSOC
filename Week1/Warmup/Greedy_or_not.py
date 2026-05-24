66891303f63a069ba2096b0cdad7b1c7bde1ffa7
Divyansh0006 <divyanshkakujain@gmail.com>
2026-05-25 02:33:13 +0530

    Update Greedy_or_not.py

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,1 +1,24 @@
-# Write your code here
+def optimizer (l, r, a):
+    if (l == r):
+        return a[l]
+    else:
+        L = optimizer(l+1,r,a)
+        R = optimizer(l,r-1,a)
+        if (a[l] - L >= a[r] - R):
+            return a[l] - L
+        else:
+            return a[r] - R
+def main():
+    n = int(input())
+    board = list()
+    for i in range(0,n):
+        board.append(int(input()))
+    difference = optimizer(0,n-1,board)
+    print (difference)
+    if (difference > 0):
+        print("Player 1 Wins")
+    elif (difference == 0):
+        print("It's a draw")
+    else:
+        print("Player 2 Wins")
+main()# Write your code here
